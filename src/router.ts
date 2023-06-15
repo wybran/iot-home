9b59ba8818d193841ef35c61cc0bd62975977b9c
wybran <krystian.wybranowski@gmail.com>
2023-06-15 10:27:44 +0200

    basic post endpint with Authorization header

--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,22 @@
 import { Router } from 'itty-router';
 
-// now let's create a router (note the lack of "new")
 const router = Router();
 
-// GET collection index
-router.get('/api/todos', () => new Response('Todos Index!'));
+router.get('/api/:device', () => new Response('Todo'));
 
-// GET item
-router.get('/api/todos/:id', ({ params }) => new Response(`Todo #${params.id}`));
-
-// POST to the collection (we'll use async here)
-router.post('/api/todos', async (request) => {
-	const content = await request.json();
-
-	return new Response('Creating Todo: ' + JSON.stringify(content));
+router.post('*', async (request, env) => {
+	const authHeader = await request.headers.get('Authorization');
+	if (authHeader !== env.AUTH_TOKEN)
+		return new Response('Unauthorized', { status: 401 });
 });
 
-// 404 for everything else
+router.post('/api/:device', async (request) => {
+	const content = await request.json();
+	console.log(request.params.device);
+	console.log(content);
+	return new Response(JSON.stringify(content));
+});
+
 router.all('*', () => new Response('Not Found.', { status: 404 }));
 
 export default router;
