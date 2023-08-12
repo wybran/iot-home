8155f8372eb29bda7499871efbd805fe6ad368e6
wybran <krystian.wybranowski@gmail.com>
2023-08-12 16:36:11 +0200

    check flood sensor state value

--- worker/src/routes/flood.ts
+++ worker/src/routes/flood.ts
@@ -3,6 +3,11 @@ import { sendPushNotification } from '../utils';
 
 const floodRouter = Router({ base: '/api/flood' });
 
-floodRouter.post('/', async (request, env) => sendPushNotification(env));
+floodRouter.post('/', async (request, env) => {
+	const content = await request.json();
+	const state = content.state as number;
+	if (state !== 1) return Response.json({ message: 'No flood detected' });
+	return sendPushNotification(env);
+});
 
 export default floodRouter;
